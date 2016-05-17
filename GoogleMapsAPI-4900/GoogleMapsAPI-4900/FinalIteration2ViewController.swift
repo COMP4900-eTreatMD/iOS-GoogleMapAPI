b9e4efeaecaf16a7a1096bfdc542097c37d8b60a
Jan Ycasas <ycasas.jan@gmail.com>
2016-05-17 14:42:08 -0700

    added json object

--- GoogleMapsAPI-4900/GoogleMapsAPI-4900/FinalIteration2ViewController.swift
+++ GoogleMapsAPI-4900/GoogleMapsAPI-4900/FinalIteration2ViewController.swift
@@ -98,6 +98,23 @@ class FinalIteration2ViewController: UIViewController,
         KRProgressHUD.show()
         
         // do recomended request
+        if let path = NSBundle.mainBundle().pathForResource("temp", ofType: "json") {
+            do {
+                let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
+                let jsonObj = JSON(data: data)
+                if jsonObj != JSON.null {
+                    print("jsonData:\(jsonObj)")
+                } else {
+                    print("could not get json from file, make sure that file contains valid json.")
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        } else {
+            print("Invalid filename/path.")
+        }
+        
+        
         
         dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_LOW, 0), {
             // Do something...
